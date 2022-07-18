a7b77818c9fb39e8c9c27702a83edf6ecfcbcc6c
jsfix.live analysis <info@coana.tech>
2022-07-18 11:35:55 +0200

    JSFIX patches

--- src/RouterProvider/RouterProvider.jsx
+++ src/RouterProvider/RouterProvider.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import createHistory from 'history/createHashHistory';
+import { createHashHistory as createHistory } from 'history';
 
 export const routerContextTypes = {
 	router: PropTypes.shape({
